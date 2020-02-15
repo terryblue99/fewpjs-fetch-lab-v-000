d3bcd786b5e1da8ae8193746f1ef7b157b02a838
Terry Blue <terryblue@comcast.net>
2020-02-15 23:31:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(json => console.log(json))
+  .then(renderBooks(resp))
 }
 
 function renderBooks(json) {
